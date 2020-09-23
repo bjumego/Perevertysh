7c82f2431615dae81097c7a64ee5ee7c541566bb
Sergei <sergei.smirnov001@ivkhk.ee>
2020-09-23 10:13:57 +0300

    rabotaet

--- src/perevertysh/App.java
+++ src/perevertysh/App.java
@@ -5,10 +5,36 @@
  */
 package perevertysh;
 
+import java.util.Scanner;
+
 /**
  *
  * @author user
  */
 class App {
+
+    private String rChStr;
+    public void run(){
+        System.out.println("=== Перевертыш ===");
+        //просим пользователя ввести строку
+        System.out.println("Введите строку");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        //полученную строку преобразуем в массив char[]
+        char[] chStr = str.toCharArray();
+        //создадим новый массив для обратной строки
+        char[] rChStr = new char[str.length()];
+        //в цикле пройдем по chStr и поместим 
+        //из chStr символы в обратном порядке
+        for (int i = 0; i < rChStr.length; i++) {
+                rChStr[i] = chStr[(chStr.length - 1) - i];
+        }
+        //преобразуем массив символов rChStr в строку
+        String rStr = new String(rChStr);
+        System.out.println("Перевертыш: "+rStr);
+        //решение с помощью класса StringBuilder
+       
+   
+    }
     
 }
